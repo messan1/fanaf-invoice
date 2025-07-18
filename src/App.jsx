a9eb3068ef9c4b1e01a9c0826cb91de6b14081ac
christian Messan <christian@muxxus.com>
2025-07-18 05:30:23 +0000

    fit

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ useGLTF.preload(
 // Function to generate Bannerbear URL with user data
 function generateBannerbearURL(name, position) {
   const baseURL =
-    "https://ondemand.bannerbear.com/simpleurl/Pa37MZkebYndyAorDp/image";
+    "https://ondemand.bannerbear.com/simpleurl/qNnLwZdMnyMB1AWDJy/image";
 
   // URL encode the values
   const encodedName = encodeURIComponent(name || "Name");
